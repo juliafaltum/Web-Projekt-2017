a5a20d7ff3f55ff0ba2b7165f56b75aa9b0d1a10
molin <molinamary000@gmail.com>
2017-05-01 18:22:53 +0200

    Edit von text, upload von Bild,, und speichern button--> speichern funktion fehlt noch

--- edit.php
+++ edit.php
@@ -17,18 +17,39 @@ if ($zeile = $query->fetchObject()) {
     echo "<h1>Tweet Nummer: $zeile->contentID<br></h1>";
     echo "<h3>Geschrieben am: $zeile->contentDate</h3>";
     if (!empty($zeile->contentTXT)) {
-        $var = $zeile->contentTXT;
+        $contentText = $zeile->contentTXT;
     }
-    echo '<input type ="text" size="80" maxlength="500" value = "'.$var.'"> <br>';
-    echo "<img src='$zeile->contentPicture' alt=\"Mountain View\" style=\"width:304px;height:228px;\"> <br>";
 
-    echo '<input type= "file" name = "fileToUpload" id="fileToUpload"><br>';  // select image to upload
+    if (!empty($zeile->contentPicture)) {
+        $contentPicture = $zeile->contentPicture;
+    }
 
-    echo "Quelle: <a href='$zeile->contentSource'>$zeile->contentSource</a><br><br>";
+
+    echo '<input type ="text" size="80" maxlength="500" value = "'.$contentText.'" id=contextText> <br>';
+    echo "<img src='$contentPicture' alt=\"Mountain View\" style=\"width:304px;height:228px;\"> <br>";
+
+    echo  '<form action= "uploadFile.php" method = "post" enctype="multipart/form-data"><br>';
+    echo '<input type= "file" name = "fileToUpload" id="fileToUpload"><br><br>';  // select image to upload
+    echo '<input type="submit" value="Upload" name="upload"/><br><br>';
+    echo  '</form>';
+
+    if (!empty($zeile->contentSource)) {
+        $contentSource = $zeile->contentSource;
+    }
+
+   echo '<input type ="text" size="80" maxlength="500" value = "'.$contentSource.'" id=contextSource> <br>';
+
+
+
+    /*echo "Quelle: <a href='$zeile->contentSource'>$zeile->contentSource</a><br><br>";
     echo "<a href='show.php?id=$zeile->contentID'>zeige</a><br>";
     echo "<a href='update_form.php?id=$zeile->contentID'>editiere</a><br>";
-    echo "<a href='delete1.php?id=$zeile->contentID'>l&ouml;sche</a><br>";
-    echo "_________________________________________________________";
+    echo "<a href='delete1.php?id=$zeile->contentID'>l&ouml;sche</a><br>";*/
+    echo "_________________________________________________________ <br><br>";
+
+    echo '<input type ="submit" value = "Save Changes" name="save"> <br>';
+
+
 } else {
     print "Datensatz mit id=$contentID nicht gefunden!";
 }
