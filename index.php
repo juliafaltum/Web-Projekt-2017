4aa32244764c51ff6183a7f533c67255611714a4
molin <molinamary000@gmail.com>
2017-05-01 14:40:57 +0200

    test4

--- index.php
+++ index.php
@@ -30,9 +30,9 @@
         echo "<h4>$zeile->contentTXT</h4>";
         echo "<img src='$zeile->contentPicture' alt=\"Mountain View\" style=\"width:304px;height:228px;\"> <br>";
         echo "Quelle: <a href='$zeile->contentSource'>$zeile->contentSource</a><br><br>";
-        echo "<a href='show.php?contentID=$zeile->contentID'>zeige</a><br>";
-        echo "<a href='update_form.php?id=$zeile->contentID'>editiere</a><br>";
-        echo "<a href='delete1.php?id=$zeile->contentID'>l&ouml;sche</a><br>";
+        echo "<a href='show.php?contentID=$zeile->contentID'>zeigen</a><br>";
+        echo "<a href='update_form.php?id=$zeile->contentID'>editieren</a><br>";
+        echo "<a href='delete1.php?id=$zeile->contentID'>l&ouml;schen</a><br>";
         echo "_________________________________________________________";
     }
     ?>
