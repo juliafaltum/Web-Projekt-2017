a986c194c43a486fda81bc32ca608937743f1407
molin <molinamary000@gmail.com>
2017-05-03 14:45:08 +0200

    edit_do.. update_form

--- edit.php
+++ edit.php
@@ -13,8 +13,10 @@ $db = new PDO($dsn, $dbuser, $dbpass);
 $sql = "SELECT * FROM content_txt WHERE contentID=$contentID";
 $query = $db->prepare($sql);
 $query->execute();
+
 if ($zeile = $query->fetchObject()) {
-    echo  '<form action="edit_do.php" method = "post" enctype="multiformular von html fängt hier an. Get und post! Bilder mit encriptet data.part/form-data"><br>';   //
+
+    echo '<form action="edit_do.php" method = "post"><br>';
     echo "<input type='hidden' name='id' value='$zeile->contentID' />";
 
     echo "<h1>Tweet Nummer: $zeile->contentID<br></h1>";
@@ -23,60 +25,28 @@ if ($zeile = $query->fetchObject()) {
     if (!empty($zeile->contentTXT)) { // confirmation of an empty database
         $contentText = $zeile->contentTXT;
     }
-
     if (!empty($zeile->contentPicture)) {
         $contentPicture = $zeile->contentPicture;
     }
-
-
     if (!empty($zeile->contentID)) {
         $contentID = $zeile->contentPicture;
     }
-
-    echo '<input type ="text" size="80" maxlength="500" value = "'.$contentText.'" id="contextText" name="contextText"> <br>';
+    echo '<input type ="text" size="80" maxlength="300" value = "' . $content_txt . '" id="content_txt" name="content_txt"> <br>';
     echo "<img src='$contentPicture' alt=\"Mountain View\" style=\"width:304px;height:228px;\"> <br>";
 
-
     echo '<input type= "file" name = "fileToUpload" id="fileToUpload"><br><br>';  // select image to upload
-   // echo '<input type="submit" value="Upload" name="upload"/><br><br>';
-
+    // echo '<input type="submit" value="Upload" name="upload"/><br><br>';
+}
 
     if (!empty($zeile->contentSource)) {
         $contentSource = $zeile->contentSource;
     }
 
-   echo '<input type ="text" size="80" maxlength="500" value = "'.$contentSource.'" id="contextSource" name="contextSource"> <br>';
+    echo '<input type ="text" size="80" maxlength="500" value = "' . $contentSource . '" id="contextSource" name="contextSource"> <br>';
 
     echo "_________________________________________________________ <br><br>";
 
     echo '<input type ="submit" value = "speichern" name="submit" id="submit"> <br>';
     echo '<input type = "submit" value = "abbrechen" name= "cancel" id="cancel"> <br>';
-    echo  '</form>';
-
-if(isset($_POST["submit"])){
-    // echo $fileToUpload_dir;
-    echo $fileToUpload_name;
-    echo $fileToUpload_extension;
-    echo $contextText;
-    echo $contextSource;
-
-  // $contentID = (int)$_GET["contentID"];
-    $db = new PDO($dsn, $dbuser, $dbpass);
-    $sql = "UPDATE content_txt SET contentDate = '20160523091528' WHERE contentID=1"; // neues datum wird nur angezeigt wenn man züruck geht und wieder den Post sieht! muss noch gemacht werden!
-    $query = $db->prepare($sql);
-    $query->execute();
-
-}
-  // aca es donde se debe poner el code de UPDATE
-
-    /// UPDATE content_txt SET contentDate = '29-04-90'
-
-} else {
-    print "Datensatz mit id=$contentID nicht gefunden!";
-}
-$db = null;
-?>
-
-<br>
-<a href="index.php">zurück</a>
+    echo '</form>';
 
