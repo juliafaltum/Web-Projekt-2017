6102bbb2285eb46f03709ca7d38ed0cf6accc9e7
molin <molinamary000@gmail.com>
2017-05-08 13:53:09 +0200

    Edit.form version 3

--- edit.php
+++ edit.php
@@ -1,55 +1,59 @@
 <?php
 include_once("session_check.php");
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="mystyle.css" media="screen"/>
 </head>
+<body>
 
 <?php
-include_once("userdata.php");
+try {
+    include_once("userdata.php");
+    $contentID = (int)$_GET["contentID"];
+    $db = new PDO($dsn, $dbuser, $dbpass);
+    $sql = "SELECT * FROM content_txt WHERE contentID=$contentID";
+    $query = $db->prepare($sql);
+    $query->execute();
+    if ($zeile = $query->fetchObject()) {
 
-$contentID = (int)$_GET["contentID"];
-$db = new PDO($dsn, $dbuser, $dbpass);
-$sql = "SELECT * FROM content_txt WHERE contentID=$contentID";
-$query = $db->prepare($sql);
-$query->execute();
+        echo "<form action='edit_do.php' method='post'>";
+        echo "<input type='hidden' name='id' value='$zeile->contentID' />";
 
-if ($zeile = $query->fetchObject()) {
+        echo "<h1>Tweet Nummer: $zeile->contentID</h1>";
+        echo "<h3>Geschrieben am: $zeile->contentDate</h3><br>";
 
-    echo '<form action="edit_do.php" method = "post"><br>';
-    echo "<input type='hidden' name='id' value='$zeile->contentID' />";
+        if (!empty($zeile -> contentTXT))
+        {$contentext = $zeile->contentTXT;}
+        if (!empty($zeile -> contentPicture))
+        {$contentPicture = $zeile->contentPicture;}
+        if (!empty($zeile -> contentID))
+        {$contentID  = $zeile->contentID;}
 
-    echo "<h1>Tweet Nummer: $zeile->contentID<br></h1>";
-    echo "<h3>Geschrieben am: $zeile->contentDate</h3>";
+        echo "<input type ='text' size='80' maxlength='300' value =' . $contentTXT . ' id= 'contentText' name= 'contentText'> <br>";
+        echo "<img src='$contentPicture' alt=\"Mountain View\" style=\"width:304px;height:228px;\"> <br>";
+        echo "<input type = 'file' name= 'fileToUpload' id= 'fileToUpload'><br><br>";
 
-    if (!empty($zeile->contentTXT)) { // confirmation of an empty database
-        $contentText = $zeile->contentTXT;
+        echo "<input type='text' name='text' size='40' maxlength='80' value='$zeile->text' /><br><br>";
+        echo "<input type ='text' size='80' maxlength='500' value = ' . $contentSource . ' id= 'contentSource' name='contentSource'> <br>";
+
+        echo "<input type='submit' value='speichern' name=' submit' id='submit'><br>";
+        echo "</form>";
+
+
+    } else {
+        echo "Datensatz nicht gefunden!";
     }
-    if (!empty($zeile->contentPicture)) {
-        $contentPicture = $zeile->contentPicture;
-    }
-    if (!empty($zeile->contentID)) {
-        $contentID = $zeile->contentPicture;
-    }
-    echo '<input type ="text" size="80" maxlength="300" value = "' . $content_txt . '" id="content_txt" name="content_txt"> <br>';
-    echo "<img src='$contentPicture' alt=\"Mountain View\" style=\"width:304px;height:228px;\"> <br>";
-
-    echo '<input type= "file" name = "fileToUpload" id="fileToUpload"><br><br>';  // select image to upload
-    // echo '<input type="submit" value="Upload" name="upload"/><br><br>';
+    $db = null;
+} catch (PDOException $e) {
+    echo "Error!: Bitten wenden Sie sich an den Administrator...";
+    die();
 }
+?>
 
-    if (!empty($zeile->contentSource)) {
-        $contentSource = $zeile->contentSource;
-    }
 
-    echo '<input type ="text" size="80" maxlength="500" value = "' . $contentSource . '" id="contextSource" name="contextSource"> <br>';
-
-    echo "_________________________________________________________ <br><br>";
-
-    echo '<input type ="submit" value = "speichern" name="submit" id="submit"> <br>';
-    echo '<input type = "submit" value = "abbrechen" name= "cancel" id="cancel"> <br>';
-    echo '</form>';
-
+</body>
+</html>
