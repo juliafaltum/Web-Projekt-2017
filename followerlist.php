0dbd01797a8934527aba20b09512977145197176
juliafaltum <julia.faltum@web.de>
2017-06-26 17:08:20 +0200

    Follower-Listen können jetzt angezeigt werden

--- followerlist.php
+++ followerlist.php
@@ -12,18 +12,17 @@ $query->bindParam(':festgelegteUserID', $festgelegteUserID);
 $query->execute();
 
 while ($zeile = $query->fetchObject()) {
-    echo "<h1>$zeile->username abonniert diese Nutzer:<br></h1>";
+    echo "<h1>$zeile->username wird von diesen Nutzern abonniert:<br></h1>";
     echo "<a href='profil.php?userid=$zeile->userid'>Zurück zum Profil</a>";
 }
 
 echo "<br><br>";
-
 $db = new PDO($dsn, $dbuser, $dbpass);
-$sql = "SELECT * FROM followerlist INNER JOIN user ON followerlist.follower=user.userid WHERE followerlist.user = :festgelegteUserID";       // UserID = 19 zeigt alles von Nutzer 19 an
+$sql = "SELECT * FROM followerlist INNER JOIN user ON followerlist.user=user.userid WHERE followerlist.follower = :festgelegteUserID";       // UserID = 19 zeigt alles von Nutzer 19 an
 $query = $db->prepare($sql);
 $query->bindParam(':festgelegteUserID', $festgelegteUserID);
 $query->execute();
 
 while ($zeile = $query->fetchObject()) {
-    echo "<a href='profil.php?userid=$zeile->userid'>$zeile->username</a></h1><br>";
+    echo "<a href='profil.php?userid=$zeile->userid'>$zeile->username</a><br>";
 }
