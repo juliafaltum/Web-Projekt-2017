5d85e1cf49de9d7b95f2e2ebbb1d8bc1a8349455
molin <molinamary000@gmail.com>
2017-06-27 15:24:53 +0200

    Suche.php  Suche_do.php

---
+++ suche_do.php
@@ -0,0 +1,44 @@
+<link rel="stylesheet" type="text/css" href="mystyle.css" media="screen"/>
+
+<h1>
+    <?php
+    $suchbegriff = $_POST['suchbegriff']; #Suchbegriff aus Formular dem Parameter $suchbegriff zuweisen
+    echo "Deine Suchergebnisse für: ". $suchbegriff?>
+</h1>
+
+<?php
+
+
+include_once("userdata.php");
+
+try {
+    $db = new PDO($dsn, $dbuser, $dbpass);
+    $sql = "SELECT * FROM user WHERE username=$suchbegriff";
+    $query = $db->prepare($sql);
+    $query->execute();
+
+    while ($zeile = $query->fetchObject()) {
+        echo "<tr>";
+        echo "<td>$zeile->username</td>";
+
+    }
+
+} catch (PDOException $e) {
+    echo "Error!: Bitte wenden Sie sich an den Administrator!?..." . $e;
+    die();
+}
+
+?>
+
+</tbody>
+</table>
+</br>
+<a href="index.php">Zurück zur Startseite</a>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: molin
+ * Date: 19.06.2017
+ * Time: 12:01
+ */
---
+++ suchusername.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Suchen</title>
+</head>
+<html>
+<body>
+<title>Username suchen</title>
+<form method="Post" action="suche_do.php">
+    <p><input name="suchbegriff" type="text">
+        <input type="submit" value="suchen"></form>
+<a href="index.php">zurück zur Startseite</a>
+</body></html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: molin
+ * Date: 19.06.2017
+ * Time: 12:59
+ */
