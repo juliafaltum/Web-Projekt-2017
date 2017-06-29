12cfb42149ee4a3577e8aed6455e2ea753586003
juliafaltum <julia.faltum@web.de>
2017-06-29 17:04:13 +0200

    Erster Versuch der Navigationsleiste

---
+++ navigation.php
@@ -0,0 +1,46 @@
+<?php
+include_once ("header.php")
+?>
+
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">
+                <img alt="ola" src="...">
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="hoechstewellen.php">Höchste Wellen<span class="sr-only">(current)</span></a></li>
+            </ul>
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Benutzer suchen">
+                </div>
+                <button type="submit" class="btn btn-default">Suchen</button>
+            </form>
+            <div class="navbar-right" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                <li class="dropdown navbar-right">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="profil.php?userid=">Profil anzeigen</a></li>
+                        <li><a href="profil_edit.php">Profil bearbeiten</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="logout.php">Abmelden</a></li>
+                    </ul>
+                </li>
+            </form>
+            </div>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
