154e8d828a2e995c340765752a689578e27fb6b0
molin <molinamary000@gmail.com>
2017-07-11 21:44:06 +0200

    Birthdate und gender! test!3

--- create_user_do.php
+++ create_user_do.php
@@ -40,8 +40,8 @@ if (!empty($username) && !empty($fullname) && !empty($email)) {
     try {
         $db = new PDO($dsn, $dbuser, $dbpass);
         $query = $db->prepare(
-            "INSERT INTO user (username, fullname, email, password, usercreated, profilePicture ) VALUES(:username, :fullname, :email, :password, NOW(), :profilePicture)");
-        $query->execute(array("username" => $username, "fullname" => $fullname, "email" => $email, "password" => $password, "profilePicture" => $uploadfile) );
+            "INSERT INTO user (username, fullname, email, password, usercreated, profilePicture, Birthdate, gender ) VALUES(:username, :fullname, :email, :password, NOW(), :profilePicture, :Birthdate, :gender)");
+        $query->execute(array("username" => $username, "fullname" => $fullname, "email" => $email, "password" => $password, "profilePicture" => $uploadfile, "Birhtdate" => $Birthdate, "gender"=> $gender ) );
         $db = null;
     } catch (PDOException $e) {
         echo "Error!: Bitten wenden Sie sich an den Administrator...";
